eaac192da31cb4247f86d9fb1753a26f81582527
Rodrigo Souza <rodrigo.rcs@gmail.com>
2022-10-23 11:47:58 -0300

    configurações do bando de dados no arquivo cypress.json

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -22,13 +22,9 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 
-  const pool = new Pool({
-    host: 'castor.db.elephantsql.com',
-    user: 'kqmmvvmx',
-    password: 'uCZbdmeF7r1Uwi_UVLhmXbmwAcGz_2YC',
-    database: 'kqmmvvmx',
-    port: 5432
-  })
+  const configJson = require(config.configFile)
+
+  const pool = new Pool(configJson.dbConfig)
 
   on('task',{
     removeUser(email) {
